7f1479afdc5b9c0e20197410683b37c173d9dc2f
Ezema Anthony Sunday <tonyunday@gmail.com>
2022-10-18 16:36:51 -0700

    his changes

---
+++ Videos/Tutorials/MICROVERSE/js-basics/index.js
@@ -0,0 +1,18 @@
+// import Book from "./book.js";
+// Book param order: cover, pages, contents, author, published
+// const evoteck = new book(
+//   "evoteck",
+//   20,
+//   "table of content",
+//   "Ezema Anthony Sunday",
+//   march2022,
+// );
+
+// console.log("The everydayBook object:", everydayBook);
+
+const content = `
+<h1>My Name is Anthony</h1>
+<p>I was born in the year 1993</p>
+`;
+
+document.body.innerHTML = content;
